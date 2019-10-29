78502116abd85298bfd44227666405132eda7030
danielgreg2 <gdecanio@ufl.edu>
2019-10-29 17:43:03 -0400

    Added About Page view, which canbe accessed at /About

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LandingPage from "./views/landingPage";
 import NotFoundPage from "./views/notFoundPage";
+import AboutPage from "./views/aboutPage";
+
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={LandingPage} />
+        <Route path="/About" exact component={AboutPage}/>
         <Route component={NotFoundPage} />
       </Switch>
     </BrowserRouter>
---
+++ client/src/views/aboutPage.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Header from "../components/Header";
+import Card from "../components/Card";
+import AboutProduct from "../components/AboutProduct";
+import "../App.css";
+import "../bootstrap.min.css";
+
+//changed
+function aboutPage() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+      <div
+        className="Body"
+        style={{
+          paddingTop: "100px",
+          backgroundColor: "black",
+          paddingBottom: "100px"
+        }}
+      >
+        <AboutProduct />
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">
+                <Card />
+              </th>
+              <th scope="col">
+                <Card />
+              </th>
+              <th scope="col">
+                <Card />
+              </th>
+              <th scope="col">
+                <Card />
+              </th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default aboutPage;
