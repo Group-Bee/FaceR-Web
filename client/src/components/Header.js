55f94ae0017bf144a11e6ad010b7de32a384e507
Jhanava Ramisetty <Jramisetty@ufl.edu>
2019-10-30 19:37:18 -0400

    Button now routes to About Page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -23,22 +23,22 @@ export class Header extends Component {
           <div className="collapse navbar-collapse" id="navbarColor01">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="www.google.com">
+                <a className="nav-link" href="http://localhost:3000/">
                   Home <span className="sr-only">(current)</span>
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="www.google.com">
+                <a className="nav-link" href="http://localhost:3000/Features">
                   Features
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="www.google.com">
+                <a className="nav-link" href="http://localhost:3000/Product">
                   Product
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="www.google.com">
+                <a className="nav-link" href="http://localhost:3000/About">
                   About
                 </a>
               </li>
