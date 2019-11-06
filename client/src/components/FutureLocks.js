3e8cf86feb5899068b48056e4ba7d312373c14b3
philiplaussermair <32105925+philiplaussermair@users.noreply.github.com>
2019-11-06 15:46:43 -0500

    first steps of future lock

---
+++ client/src/components/FutureLocks.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export class FutureLocks extends Component {
+  render() {
+
+    return (
+    <div class="card">
+    <div class="card-body">
+
+        <h1>The Future of Locks</h1>
+
+        <hr class="new" width="50%"></hr>
+
+        The future of locks is ever changing in the new Technological Age. With ease being as important as the security aspect now, FaceR is the next step in where locks are going.
+    </div>
+  </div>
+    );
+  }
+
+}
+export default FutureLocks;
