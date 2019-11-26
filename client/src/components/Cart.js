f065b2d63b5177a188c1d99900fdd2c945ac2db5
tiki <tikaharik@gmail.com>
2019-11-26 11:28:46 -0500

    Payment page > Cart dropdown > Added remove button / fixed functionality

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -29,36 +29,36 @@ class Cart extends Component {
     }
     console.log('incart cart.js', this.props.incart.length)
 
-    let dropdown = this.props.incart.map(element =>{
-      return(
-              <li className="item" onClick={this.props.remove.bind(this, element)}>
-                <p style={{textAlign:'center'}} className="p-item">
-                  <span style={{float: 'left'}}>{element.name}</span>
-                  &emsp;......&emsp;
-                  <span style={{float: 'right'}}>{element.price}</span>
-                  </p>
-            </li>
+    let dropdown = this.props.incart.map(element => {
+      return (
+        <li className="item"  style={{whiteSpace: 'nowrap'}}>
+          <p style={{ textAlign: 'center', whiteSpace: 'nowrap', overflow: 'hidden'}} className="p-item">
+            <span style={{ float: 'left' }}>&emsp;&emsp;{element.name}</span>
+            &emsp;
+                  <span style={{ float: 'right' }}>{element.price}&emsp;<button onClick={this.props.remove.bind(this, element)} style={{backgroundColor: 'salmon', borderRadius: '6px', float: 'right'}}>Remove</button></span>
+          </p>
+        </li>
       )
     })
     return (
       <div>
         <li className="nav-right" style={{ zIndex: '2' }}>
           <div style={{ backgroundColor: '#1a1a1a' }}>
-            <button
-              style={{ backgroundColor: '#1a1a1a', color: 'white' }}
-              onClick={this.showCart.bind(this)}
-            >
-              <i className="fa fa-shopping-cart fa-2x" aria-hidden="true"></i>
-              &nbsp;&nbsp;&nbsp;<i className="fa fa-caret-down"></i></button>
+
+            <i className="fa fa-shopping-cart fa-2x" aria-hidden="true" style={{cursor: 'auto'}}></i>
+            &nbsp;        <button
+              style={{ backgroundColor: '#1a1a1a', color: 'white'}}
+              onClick={this.showCart.bind(this)}>
+                <i className="fa fa-caret-down"></i></button>
           </div>
         </li>
         {(this.state.showCart && this.props.incart.length != 0) ?
-          (<div className="mydrop" style={{ backgroundColor: 'white', color: 'black', position: 'absolute', zIndex: '100' }}>
+          (<div className="mydrop" style={{ backgroundColor: 'white', color: 'black', position: 'absolute', zIndex: '100', borderTopLeftRadius: '6px', borderTopRightRadius: '6px' }}>
             <ul className="myul" style={{ listStyle: 'none' }}>
               {dropdown}
-              <li className="item" style={{textAlign: 'center', backgroundColor: 'grey', fontFamily: 'Times New Roman'}}>
-              <a className="nav-link" href="ConfirmPayment" style={{color: "white"}}>
-                Confirm Payment
+              <li className="item" style={{ textAlign: 'center', backgroundColor: 'grey', fontFamily: 'Times New Roman' }}>
+                <a className="nav-link" href="ConfirmPayment" style={{ color: "white"}}>
+                  Confirm Payment
                 </a>
               </li>
             </ul>
