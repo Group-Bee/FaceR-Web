94b51238c4a834c1afbf3345a2cbcb85ddabde2a
danielgreg2 <gdecanio@ufl.edu>
2019-11-30 13:02:10 -0500

    About Page: Testimonial Carousel: Functionality: Testimonials are now stored in a data file and information is referenced from that data file (FRONTEND IMPLEMENTATION)

--- client/src/components/TestimonialsCarousel.js
+++ client/src/components/TestimonialsCarousel.js
@@ -1,52 +1,34 @@
 import React, { Component } from "react";
 import Carousel from 'react-bootstrap/Carousel';
+import data from '../data/testimonial.data'
 
 //Bootstrap documentation on how to create carousel form scratch: https://getbootstrap.com/docs/4.0/components/carousel/
 //Bootstrap documentation on how to create carousel using pre-made bootstrap components: https://react-bootstrap.github.io/components/carousel/
 
+/*
+Frontend implementation of testimonials carousel.
+Data for testimonials is stored at client/src/data/testimonial.data.js. If you want to adjust the data being displayed, change that file.
+*/
 export default class TestimonialsCarousel extends Component {
   render() {
+    //To change the number of testimonials being displayed, adjust the 2nd number in the 'slice()' function
+    const testimonials = data.slice(0,3).map(element => {
+      return(
+        <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="images/CyberLock_Cropped.jpeg"
+          alt="Testimonial"
+        />
+        <Carousel.Caption>
+          <h3 class="row justify-content-start text-white">{element.testimonial}}</h3>
+          <p class="row justify-content-start">-{element.name}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      )
+    })
     return (
-        <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            //src="/images/CyberLock.jpg"
-            src="images/CyberLock_Cropped.jpeg"
-            alt="Testimonial"
-          />
-          <Carousel.Caption>
-            <h3 class="row justify-content-start text-white">First customer testimonial</h3>
-            <p class="row justify-content-start">-1st customer name</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            //src="/images/CyberLock.jpg"
-            src="images/CyberLock_Cropped.jpeg"
-            alt="Testimonial"
-          />
-      
-          <Carousel.Caption>
-            <h3 class="row justify-content-start text-white">Second customer testimonial</h3>
-            <p class="row justify-content-start">-2nd customer name</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            //src="/images/CyberLock.jpg"
-            src="images/CyberLock_Cropped.jpeg"
-            alt="Testimonial"
-          />
-      
-          <Carousel.Caption>
-            <h3 class="row justify-content-start text-white">Third customer testimonial</h3>
-            <p class="row justify-content-start">-3rd customer name</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+    <Carousel>{testimonials}</Carousel>
     );
   }
 }
