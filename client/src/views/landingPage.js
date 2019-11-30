a1f5050895883fc5433539655c1e7f3a1b7083c4
Aiden Dowell <Adonamor@gmail.com>
2019-11-30 16:06:05 -0500

    Landing Page Header: Functionality: oppacity transitions to 0 while at top of page

--- client/src/views/landingPage.js
+++ client/src/views/landingPage.js
@@ -8,38 +8,82 @@ import "../bootstrap.min.css";
 
 //changed
 class landingPage extends React.Component {
-  render(){
-  return (
-    <div className="App">
-      <header className="App-header" id="nav">
-        <Header  incart={this.props.incart} add={this.props.add.bind(this)} remove={this.props.remove.bind(this)} />
-      </header>
-      <div
-        className="Body"
-        style={{
-          paddingTop: '0px',
-          backgroundColor: 'black',
-          paddingBottom: '20px'
-        }}
+  state = {
+    atTop: true
+  };
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.onScroll);
+  }
+
+  onScroll = () => {
+    console.log(window.pageYOffset);
+    if (window.pageYOffset <= 60) {
+      this.setState({ atTop: true });
+    } else {
+      this.setState({ atTop: false });
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <header
+          style={
+            this.state.atTop
+              ? {
+                  opacity: "1",
+                  transition: "opacity 1s",
+                  opacity: "0"
+                }
+              : { transition: "opacity 1s", opacity: "1" }
+          }
+          className="App-header"
+          id="nav"
         >
-          <p className="lead" style={{width: '100%', height: '900px', position: 'relative', zIndex: '0', marginBottom: '-100px'}}>
+          <Header
+            incart={this.props.incart}
+            add={this.props.add.bind(this)}
+            remove={this.props.remove.bind(this)}
+          />
+        </header>
+        <div
+          className="Body"
+          style={{
+            paddingTop: "0px",
+            backgroundColor: "black",
+            paddingBottom: "20px"
+          }}
+        >
+          <p
+            className="lead"
+            style={{
+              width: "100%",
+              height: "900px",
+              position: "relative",
+              zIndex: "0",
+              marginBottom: "-100px"
+            }}
+          >
             <iframe
-            title="productVid"
-            width= '100%'
-            height= '100%'
-            src="https://www.youtube.com/embed/1XZZpf6yReg?&rel=0&loop=1&controls=0&autoplay=1"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
+              title="productVid"
+              width="100%"
+              height="100%"
+              src="https://www.youtube.com/embed/1XZZpf6yReg?&rel=0&loop=1&controls=0&autoplay=1"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
             ></iframe>
           </p>
 
-        <AboutProduct />
+          <AboutProduct />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default landingPage;
