f6c22ea69e427d2844814f1943feb207448a4cb5
DESKTOP-26HBAMS\brett <coiniichu@gmail.com>
2019-12-02 01:25:41 -0500

    Made Shopping Cart Background Transparent

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -3,69 +3,115 @@ import Stripe from "react-stripe-checkout";
 
 class Cart extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       showCart: false
-    }
+    };
   }
 
   showCart = () => {
     this.setState({
       showCart: true
-    })
-    document.addEventListener('click', this.hideCart)
-  }
+    });
+    document.addEventListener("click", this.hideCart);
+  };
 
   hideCart = () => {
     this.setState({
       showCart: false
-    })
-    document.removeEventListener('click', this.hideCart)
-  }
+    });
+    document.removeEventListener("click", this.hideCart);
+  };
 
   render() {
     function handleToken(token, addresses) {
-      console.log({ token, addresses })
+      console.log({ token, addresses });
     }
-    console.log('incart cart.js', this.props.incart.length)
+    console.log("incart cart.js", this.props.incart.length);
 
     let dropdown = this.props.incart.map(element => {
       return (
-        <li className="item" style={{whiteSpace: 'nowrap'}}>
-          <p style={{ textAlign: 'center', whiteSpace: 'nowrap', overflow: 'hidden'}} className="p-item">
-            <span style={{ float: 'left' }}>&emsp;&emsp;{element.name}</span>
+        <li className="item" style={{ whiteSpace: "nowrap" }}>
+          <p
+            style={{
+              textAlign: "center",
+              whiteSpace: "nowrap",
+              overflow: "hidden"
+            }}
+            className="p-item"
+          >
+            <span style={{ float: "left" }}>&emsp;&emsp;{element.name}</span>
             &emsp;
-                  <span style={{ float: 'right' }}>{element.price}&emsp;<button onClick={this.props.remove.bind(this, element)} style={{backgroundColor: 'salmon', borderRadius: '6px', float: 'right'}}>Remove</button></span>
+            <span style={{ float: "right" }}>
+              {element.price}&emsp;
+              <button
+                onClick={this.props.remove.bind(this, element)}
+                style={{
+                  backgroundColor: "salmon",
+                  borderRadius: "6px",
+                  float: "right"
+                }}
+              >
+                Remove
+              </button>
+            </span>
           </p>
         </li>
-      )
-    })
+      );
+    });
     return (
       <div>
-        <li className="nav-right" style={{ zIndex: '2' }}>
-          <div style={{ backgroundColor: '#1a1a1a' }}>
-
-            <i className="fa fa-shopping-cart fa-2x" aria-hidden="true" style={{cursor: 'auto'}}></i>
-            &nbsp;        <button
-              style={{ backgroundColor: '#1a1a1a', color: 'white'}}
-              onClick={this.showCart.bind(this)}>
-                <i className="fa fa-caret-down"></i></button>
+        <li className="nav-right" style={{ zIndex: "2" }}>
+          <div style={{ backgroundColor: "transparent" }}>
+            <i
+              className="fa fa-shopping-cart fa-2x"
+              aria-hidden="true"
+              style={{ cursor: "auto" }}
+            ></i>
+            &nbsp;{" "}
+            <button
+              style={{ backgroundColor: "transparent", color: "white" }}
+              onClick={this.showCart.bind(this)}
+            >
+              <i className="fa fa-caret-down"></i>
+            </button>
           </div>
         </li>
-        {(this.state.showCart && this.props.incart.length != 0) ?
-          (<div className="mydrop" style={{ backgroundColor: 'white', color: 'black', position: 'absolute', zIndex: '100', borderTopLeftRadius: '6px', borderTopRightRadius: '6px' }}>
-            <ul className="myul" style={{ listStyle: 'none' }}>
+        {this.state.showCart && this.props.incart.length != 0 ? (
+          <div
+            className="mydrop"
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              position: "absolute",
+              zIndex: "100",
+              borderTopLeftRadius: "6px",
+              borderTopRightRadius: "6px"
+            }}
+          >
+            <ul className="myul" style={{ listStyle: "none" }}>
               {dropdown}
-              <li className="item" style={{ textAlign: 'center', backgroundColor: 'grey', fontFamily: 'Times New Roman' }}>
-                <a className="nav-link" href="ConfirmPayment" style={{ color: "white"}}>
+              <li
+                className="item"
+                style={{
+                  textAlign: "center",
+                  backgroundColor: "grey",
+                  fontFamily: "Times New Roman"
+                }}
+              >
+                <a
+                  className="nav-link"
+                  href="ConfirmPayment"
+                  style={{ color: "white" }}
+                >
                   Confirm Payment
                 </a>
               </li>
             </ul>
-          </div>)
-          : (null)
-        }</div>
-    )
+          </div>
+        ) : null}
+      </div>
+    );
   }
 }
 
